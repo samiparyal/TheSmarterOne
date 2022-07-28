3b5838436f5e5291aea82243c0b3698f1792aee3
Samip Aryal <84836995+samiparyal@users.noreply.github.com>
2022-07-28 11:09:21 +0545

    Code Shorten and Updated

--- servo.ino
+++ servo.ino
@@ -3,12 +3,20 @@ float echoPin=A0;
 float trigPin=A1;
 long duration;
 float distance;
-
+void servo_pwm_con(int deg, int pin)
+{
+  int val = (deg*10.25)+500; // i.e. 500 ms means 0 degree rotation whereas val = 2345 ms means high pulse for 2345 microsecs resulting 150 degree rotation at servo motor   
+  digitalWrite(pin,HIGH);
+  delayMicroseconds(val);
+  digitalWrite(pin,LOW);
+  delay(7);
+}
 void setup()
 {
   pinMode(ser1_pwm_pin, OUTPUT);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  
   Serial.begin(9600);
 }
 
@@ -22,27 +30,13 @@ void loop()
   
   duration= pulseIn(echoPin, HIGH);
   distance=duration*0.034/2;
-  Serial.print(distance);
-
-   if (distance<140)
+  Serial.println(distance);
+    if (distance<27)
   {
-    for (int deg = 0; deg < 140; deg++) 
-    { 
-      servo_pwm_con(deg,ser1_pwm_pin);              
+    servo_pwm_con(140,ser1_pwm_pin);              
+   }
+    else{
+    servo_pwm_con(0,ser1_pwm_pin);              
     }
-  if (distance>140)  
-//    delay(10000);
-    for (int deg = 140; deg > 0; deg--)  
-    { 
-      servo_pwm_con(deg,ser1_pwm_pin);               
-    }
-  }
-}
-void servo_pwm_con(int deg, int pin)
-{
-  int val = (deg*10.25)+500; // i.e. 500 ms means 0 degree rotation whereas val = 2345 ms means high pulse for 2345 microsecs resulting 150 degree rotation at servo motor   
-  digitalWrite(pin,HIGH);
-  delayMicroseconds(val);
-  digitalWrite(pin,LOW);
-  delay(1);
+   
 }
